3c44b1c1243e3f68e1c95bec1dc1b03401d4faca
User <User@DESKTOP-IV4ETGD>
2025-04-03 01:20:37 +0800

    Added FileHandler.java to read/write file & Starter code to main.java

---
+++ SC2002_FCS3_Group2/src/FileHandler.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileHandler {
+
+    public static void writeToFile(String filename, List<String> data) {
+        try (BufferedWriter bw = new BufferedWriter(new FileWriter(filename))) {
+            for (String line : data) {
+                bw.write(line);
+                bw.newLine();
+            }
+        } catch (IOException e) {
+            System.out.println("Error writing to file: " + e.getMessage());
+        }
+    }
+
+    // Generic method to read a file and return a list of strings
+    public static List<String> readFromFile(String filename) {
+        List<String> data = new ArrayList<>();
+        try (BufferedReader br = new BufferedReader(new FileReader(filename))) {
+            String line;
+            while ((line = br.readLine()) != null) {
+                data.add(line);
+            }
+        } catch (IOException e) {
+            System.out.println("Error reading from file: " + e.getMessage());
+        }
+        return data;
+    }
+}
--- SC2002_FCS3_Group2/src/main.java
+++ SC2002_FCS3_Group2/src/main.java
@@ -1,9 +1,58 @@
+import java.util.*;
 
 public class main {
 
+	private static final String[] USER_FILES = {"data/ApplicantList.csv"};//,"data/ManagerList.csv","data/OfficerList.csv"};
+    private static final String[] PROJECT_FILES = {"data/Projects.csv"};
+    
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		try {
+			List<User> userList = setUpUser();
+			userList.forEach(user -> System.out.println(user.toString()));
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		
 	}
+	
+	static List<User> setUpUser() {
+		
+		List<User> userList = new ArrayList<User>();
+		
+		for (String file : USER_FILES) {
+			List<User> userGroup = FileHandler.readFromFile(file).stream()
+									.skip(1)
+									.map(entry -> {
+										String[] values = entry.split(",");
+										return createUserFromFile(file,values);
+										})
+									.filter(user -> user != null)
+									.toList();
+			userList.addAll(userGroup);
+		}
+		return userList;
+		
+	}
+	static User createUserFromFile(String file, String[] values) {
+		
+	    String name = values[0];
+	    String nric = values[1];
+	    int age = Integer.valueOf(values[2]);
+	    MaritalStatus status = MaritalStatus.valueOf(values[3].toUpperCase());
+	    String password = values[4];
 
+	    switch(file) {
+	        case "data/ApplicantList.csv":
+	            return new Applicant(name, nric, age, status, password);
+	        /*case "data/ManagerList.csv":
+	            return new HDBManager(name, nric, age, status, password);
+	        case "data/OfficerList.csv":
+	            return new HDBOfficer(name, nric, age, status, password);*/
+	        default:
+	            System.out.println("Invalid File");
+	            return null;
+	    }
+	}
 }
