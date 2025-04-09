b3b319ad7940d6985ac9bc8c27f9bb97f538e749
jaxch <jaxch@LenovoLaptop>
2025-04-09 16:58:54 +0800

    Pushed viewing logic to MaritalStatus.java

--- SC2002_FCS3_Group2/src/enums/MaritalStatus.java
+++ SC2002_FCS3_Group2/src/enums/MaritalStatus.java
@@ -1,6 +1,19 @@
 package enums;
 
 public enum MaritalStatus {
-	SINGLE,
-	MARRIED
+    SINGLE {
+        @Override
+        public boolean canView(FlatType flatType, int age) {
+            return age >= 35 && flatType == FlatType.TWO_ROOM;
+        }
+    },
+    MARRIED {
+        @Override
+        public boolean canView(FlatType flatType, int age) {
+            return age >= 21 && 
+                (flatType == FlatType.TWO_ROOM || flatType == FlatType.THREE_ROOM);
+        }
+    };
+
+    public abstract boolean canView(FlatType flatType, int age);
 }
