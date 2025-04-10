78908d1f094345a77cb7a8f92bdf4c3c5ffcc652
jaxch <jaxch@LenovoLaptop>
2025-04-10 16:24:25 +0800

    Update to BTOManagementSystem.java

--- SC2002_FCS3_Group2/src/main/BTOManagementSystem.java
+++ SC2002_FCS3_Group2/src/main/BTOManagementSystem.java
@@ -1,24 +1,31 @@
 package main;
 
+import java.util.Scanner;
+
 import application.ApplicationManager;
+import interfaces.IApplicationManager;
+import interfaces.IProjectManager;
+import interfaces.IUserManager;
+import project.ProjectManager;
 import user.User;
 import user.UserManager;
 
 public class BTOManagementSystem {
-	//private List<Project> projectList;
-	private UserManager userManager;
-	private ApplicationManager applicationManager;
+	private final IUserManager userManager;
+    private final IProjectManager projectManager;
+    private final IApplicationManager applicationManager;
+	private Scanner sc;
 	
 	BTOManagementSystem(){
-		this.userManager = new UserManager();
+		userManager = new UserManager();
+		projectManager = new ProjectManager();
+		applicationManager = new ApplicationManager(projectManager,userManager);
 	}
 	
 	public void startSystem() {
-		this.userManager.initializeFromCSV();
 		
 		User user = userManager.loginUser();
         if (user != null) {
-        	//
         } else {
             System.out.println("Login failed. Exiting system.");
         }
