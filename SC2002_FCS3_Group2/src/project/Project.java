9929b7f3efa7386f87c21079b2814b8b07c4e14a
CedTheStone <49112522+Cedthestone@users.noreply.github.com>
2025-04-14 16:21:05 +0800

    Updated Project.java

--- SC2002_FCS3_Group2/src/project/Project.java
+++ SC2002_FCS3_Group2/src/project/Project.java
@@ -1,26 +1,31 @@
-package project;
+package group_project;
 
 import enums.FlatType;
 
 import java.time.LocalDate;
-import java.util.*;	
+
+import java.util.*;
+import java.util.AbstractMap.SimpleEntry;
+import java.util.Map.Entry;	
 
 public class Project {
 	protected String projectName;
 	protected String neighbourhood;
-	protected Map<FlatType, Integer> flatSupply;
+	protected EnumMap<FlatType, SimpleEntry<Integer, Double>> flatSupply;
 	protected LocalDate openingDate;
 	protected LocalDate closingDate;
 	protected HDBManager manager;
 	protected int officerSlots;
 	protected List<HDBOfficer> officerList;
 	private boolean isVisible;
-
-	public Project(String name, String area, LocalDate date1, LocalDate date2) {	//How tf are we gonna 1) ask for input and 2) input the flatype numbers???
+	
+	public Project(String name, String area, EnumMap<FlatType, SimpleEntry<Integer, Double>> flatMap, LocalDate date1, LocalDate date2, int offSlots) {
 		this.projectName = name;
 		this.neighbourhood = area;
+		this.flatSupply = flatMap;
 		this.openingDate = date1;
 		this.closingDate = date2;
+		this.officerSlots = offSlots;
 	}
 	public String getProjectName() {
 		return projectName;
@@ -31,16 +36,19 @@ public class Project {
 	public Set<FlatType> getFlatTypes() {         // For filtering
 	    return flatSupply.keySet();
 	}
-	public void printFlatSupply() {									//Do we need a getFlatSupply()?
+	public void printFlatSupply() {
 		System.out.print("Flat Types and available units:");
 		if (flatSupply.isEmpty()) {
 			System.out.print("No flats lmao");
 		}
 		else {
-			for (Map.Entry<FlatType, Integer> entry : flatSupply.entrySet()) {
+			for (Entry<FlatType, SimpleEntry<Integer, Double>> entry : flatSupply.entrySet()) {
 				System.out.print(entry.getKey()+ ": " +entry.getValue()+ " units");
 			}
 		}
+	}
+	public EnumMap<FlatType, SimpleEntry<Integer, Double>> getFlatSupply(){
+		return flatSupply;
 	}
 	public LocalDate getOpeningDate() {
 		return openingDate;
@@ -48,24 +56,27 @@ public class Project {
 	public LocalDate getClosingDate() {
 		return closingDate;
 	}
-	public int getOfficrSlots() {
+	public int getOfficarSlots() {
 		return officerSlots;
 	}
 	public String getManagerName() {
 		return manager.getName();
-	}
-	public boolean getVisibility() {
-		return isVisible;
 	}
 	public void printOfficerList() {
 		for (HDBOfficer officer : officerList) {
 			officer.getName();
 		}
 	}
+	public void addManager(HDBManager manager) {
+		this.manager = manager;
+	}
+	public void addOfficer(HBDOfficer officer) {
+		officerList.add(officer);
+		//officerslots -1 		Only if this means empty slots
+	}
 	public void toggleVisibility() {
 		isVisible = !isVisible;
 	}
-	
 	public String toString() {
 		return "Project: " + projectName
 			     + "\nNeighbourhood: " + neighbourhood
