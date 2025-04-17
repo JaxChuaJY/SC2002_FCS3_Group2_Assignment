349ec47be59e7d08edd5b168d3e1255d332f2466
CedTheStone <49112522+Cedthestone@users.noreply.github.com>
2025-04-17 20:53:31 +0800

    Added fixes and write/delete entries into csv to ProjectManager.java

--- SC2002_FCS3_Group2/src/project/ProjectManager.java
+++ SC2002_FCS3_Group2/src/project/ProjectManager.java
@@ -1,4 +1,4 @@
-package group_project;
+package project;
 
 import interfaces.IProjectManager;
 import user.User;
@@ -14,26 +14,26 @@ import java.time.format.DateTimeFormatter;
 public class ProjectManager implements IProjectManager{
 	private List<Project> projectList;
 	public String filePath = "src/ProjectList.csv";
+	private DateTimeFormatter readformat = DateTimeFormatter.ofPattern("M/d/yyyy");
 	
 	public ProjectManager() {
 		projectList = new ArrayList<>();
 		
 		try (BufferedReader br = new BufferedReader(new FileReader(filePath))) {
 			String line;
-			DateTimeFormatter format = DateTimeFormatter.ofPattern("M/d/yyyy");
 			br.readLine();				//Skip Header
 			
 			while ((line = br.readLine())!=null) {
 				String[] items = line.split(","); 
 				EnumMap<FlatType, SimpleEntry<Integer, Double>> flatMap = new EnumMap<>(FlatType.class);
 				//Mapping of FlatType with number of units + price
-				if (items[2].equalsIgnoreCase("two-room")) {
+				if (items[2].equalsIgnoreCase("2-room")) {
 					flatMap.put(FlatType.TWO_ROOM, new SimpleEntry(Integer.parseInt(items[3]), Double.parseDouble(items[4])));
 				}
 				else {
 					flatMap.put(FlatType.THREE_ROOM, new SimpleEntry(Integer.parseInt(items[3]), Double.parseDouble(items[4])));
 				}
-				if (items[5].equalsIgnoreCase("two-room")) {
+				if (items[5].equalsIgnoreCase("2-room")) {
 					flatMap.put(FlatType.TWO_ROOM, new SimpleEntry(Integer.parseInt(items[6]), Double.parseDouble(items[7])));
 				}
 				else {
@@ -42,8 +42,8 @@ public class ProjectManager implements IProjectManager{
 				
 				String name = items[0];
 				String neighbourhood = items[1];
-				LocalDate openDate = LocalDate.parse(items[8], format);
-				LocalDate closeDate = LocalDate.parse(items[9], format);
+				LocalDate openDate = LocalDate.parse(items[8], readformat);
+				LocalDate closeDate = LocalDate.parse(items[9], readformat);
 				int offSlots = Integer.parseInt(items[11]);					//Officer Slots mean empty slots left or total slots?
 				Project project = new Project(name, neighbourhood, flatMap, openDate, closeDate, offSlots);
 				if (!items[10].isEmpty()) {
@@ -93,15 +93,31 @@ public class ProjectManager implements IProjectManager{
 		else {
 			flatMap.put(FlatType.THREE_ROOM, new SimpleEntry(0, null));
 		}
-		DateTimeFormatter format = DateTimeFormatter.ofPattern("M/d/yyyy");
 		System.out.print("\nEnter Opening Date in M/D/YYYY: ");
-		LocalDate openDate = LocalDate.parse(sc.next(), format);
+		LocalDate openDate = LocalDate.parse(sc.next(), readformat);
 		System.out.print("\nEnter Closing Date in M/D/YYYY: ");
-		LocalDate closeDate = LocalDate.parse(sc.next(), format);
+		LocalDate closeDate = LocalDate.parse(sc.next(), readformat);
 		System.out.print("\nEnter number of Officer Slots: ");
 		int offSlots = sc.nextInt();
 		Project newProj = new Project(name, neighbourhood, flatMap, openDate, closeDate, offSlots);
 		newProj.addManager(manager);
+
+		try (FileWriter writer = new FileWriter("projects.csv", true)) { 
+			writer.append(newProj.getProjectName()).append(",")
+			.append(newProj.getNeighbourhood()).append(",");
+			for (FlatType type : flatMap.keySet()) {
+				SimpleEntry<Integer, Double> entry = flatMap.get(type);
+				writer.append(type.name()).append(",")
+                		.append(String.valueOf(entry.getKey())).append(",")
+                		.append(String.valueOf(entry.getValue())).append(",");
+			}
+			writer.append(newProj.getOpeningDate().toString()).append(",")
+			.append(newProj.getClosingDate().toString()).append(",")
+			.append(newProj.getManagerName()).append(",")
+			.append(Integer.toString(newProj.getOfficerSlots())).append(",")
+			.append(newProj.getOfficerList().toString())
+			.append("\n");
+		}
 	}
 	public void viewProject(User user) {
 		Project project = null;
@@ -122,6 +138,26 @@ public class ProjectManager implements IProjectManager{
 			Project proj = iter.next();
 			if (proj.getProjectName().equalsIgnoreCase(projectName)) {
 				iter.remove();
+
+				File tempFile = new File("Projects_temp.csv");
+				try (BufferedReader reader = new BufferedReader(new FileReader(filePath));
+			        BufferedWriter writer = new BufferedWriter(new FileWriter(tempFile))){
+					String currLine;
+					while ((currLine = reader.readLine()) != null) {
+						String[] items = currLine.split(",", -1);
+						if (!items[0].equalsIgnoreCase(projectName)) {
+		                    			writer.write(currLine);
+		                    			writer.newLine();
+		                		}
+					}
+				}catch (IOException e) {
+		            		e.printStackTrace();
+				}
+				if (inputFile.delete()) {
+		            	tempFile.renameTo(inputFile);
+		            	//Just to see if it works
+		            	System.out.print("Project Removed from CSV");
+		        	}
 				return true;
 			}
 		}
