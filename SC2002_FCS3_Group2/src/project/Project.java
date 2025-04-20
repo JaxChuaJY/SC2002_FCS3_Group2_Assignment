6745032ca7e28c3a1e9d7d1aac736b72ce6aa476
CedTheStone <49112522+Cedthestone@users.noreply.github.com>
2025-04-20 14:19:17 +0800

    Update Project.java

--- SC2002_FCS3_Group2/src/project/Project.java
+++ SC2002_FCS3_Group2/src/project/Project.java
@@ -32,7 +32,9 @@ public class Project {
 		this.closingDate = date2;
 		this.officerSlots = offSlots;
 		this.officerList = new ArrayList<HDBOfficer>();
-		this.isVisible = true;
+		if (openingDate.isBefore(LocalDate.now()) && closingDate.isBefore(LocalDate.now()) || openingDate.isEqual(LocalDate.now()) || closingDate.isEqual(LocalDate.now())) {
+			this.isVisible = true;
+		}
 	}
 	
 	
